fde3ee03bd483573538c82ba5c6ddc5b2fbb724b
Roszkopf Adel <133266580+radel01@users.noreply.github.com>
2024-02-15 22:27:42 +0100

    kész

---
+++ index.js
@@ -0,0 +1,21 @@
+import{ elagazas1, elagazas2, üdvözlés, ciklus1, ciklus2 } from "./fuggvenyek";
+
+// var let const - változók hatókörét jelenti
+var szam = 2;
+
+let szoveg = "Szép nap van!";
+üdvözlés();
+//3 egyenlőségjellel - típust is és az értéket is ellenőrzi
+elagazas1();
+
+//a szám változóiról döntsük el, hogy páros-e?
+elagazas2();
+//ciklus
+ciklus1()
+ciklus2()
+
+// hozz létre egy új projektet
+//index.html
+//index.js
+//fuggvenyek.js - paraméterje egy szám, 1től eddig a számig visszaadja a számokat és visszatért az összeggel
+//eredményt: index.js
