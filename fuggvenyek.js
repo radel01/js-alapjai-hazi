f1568971e04361c162f7bef848aeaaddb720b933
Roszkopf Adel <133266580+radel01@users.noreply.github.com>
2024-02-16 10:00:40 +0100

    kész

--- fuggvenyek.js
+++ fuggvenyek.js
@@ -1,38 +1,9 @@
-function elagazas2(){
-    if (szam === 0) {
-    console.log("A szám 0");
-    } else if (szam % 2 === 0) {
-    console.log(szam + " páros");
-    } else {
-    console.log(szam + " páratlan");
-    }
-}
-
-function elagazas1(){
-    if (szam === 0) {
-    console.log("A számunk = " + szam);
-    console.log(typeof szam);
-    } else {
-    console.log("A számunk = " + szam);
-    console.log(typeof szam);
-    }
-}
-
-function üdvözlés(nev) {
-    console.log("helló " + nev + " programozni jó!");
-  }
-
-function ciklus1(){
-    for (let index = 0; index < 10; index++) {
-        console.log("Ez az első üzenet!" + index);
-        console.log(`A(z) ${index + 1}. üzenet`);
-      }
-}
-
-function ciklus2(){
+export function összegzes(szam){
     let index = 0;
-    while (index < 10) {
-      index++;
-      console.log("Ez az első üzenet!" + index);
+    let ossz = 0;
+    while (index <= szam) {
+        ossz += index;
+        index++;
     }
+    return ossz;
 }
--- index.js
+++ index.js
@@ -1,21 +1,4 @@
-import{ elagazas1, elagazas2, üdvözlés, ciklus1, ciklus2 } from "./fuggvenyek";
+import {összegzes} from "./fuggvenyek.js"
 
-// var let const - változók hatókörét jelenti
-var szam = 2;
-
-let szoveg = "Szép nap van!";
-üdvözlés();
-//3 egyenlőségjellel - típust is és az értéket is ellenőrzi
-elagazas1();
-
-//a szám változóiról döntsük el, hogy páros-e?
-elagazas2();
-//ciklus
-ciklus1()
-ciklus2()
-
-// hozz létre egy új projektet
-//index.html
-//index.js
-//fuggvenyek.js - paraméterje egy szám, 1től eddig a számig visszaadja a számokat és visszatért az összeggel
-//eredményt: index.js
+console.log(összegzes(3));
+console.log(összegzes(5));
